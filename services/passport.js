1f114b3f79240837dfc61f780a2c2d8744bf3850
jackdishman <jdish123@gmail.com>
2019-10-06 17:44:30 -0500

    implemented async functions

--- services/passport.js
+++ services/passport.js
@@ -28,20 +28,17 @@ passport.use(
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback'
         },
-        (accessToken, refreshToken, profile, done) => {
+        async (accessToken, refreshToken, profile, done) => {
             console.log('profile:', profile.id);
-            User.findOne({ googleID: profile.id }).then(existingUser => {
-                if (existingUser) {
-                    //we already have a record with the given profile ID
-                    done(null, existingUser);
-                } else {
-                    new User({
-                        googleID: profile.id,
-                        name: profile.displayName
-                    }).save().then(user => done(null, user));
-                }
-            });
-
+            const existingUser = await User.findOne({ googleID: profile.id });
+            if (existingUser) {
+                //we already have a record with the given profile ID
+                done(null, existingUser);
+            } else {
+                //make a new user
+                const user = await new User({ googleID: profile.id,}).save();
+                done(null, user);
+            }
         }
     )
 );
@@ -54,18 +51,16 @@ passport.use(
             profileFields: ['id', 'displayName'],
             enableProof: true
         },
-        (accessToken, refreshToken, profile, done) => {
-            User.findOne({ facebookID: profile.id }).then(existingUser => {
-                console.log(JSON.stringify(profile));
+        async (accessToken, refreshToken, profile, done) => {
+            const existingUser = await User.findOne({ facebookID: profile.id });
                 if (existingUser) {
+                    //existinng user
                     done(null, existingUser);
                 } else {
-                    new User({
-                        facebookID: profile.id,
-                        name: profile.displayName
-                    }).save().then(user => done(null, user));
+                    //new user 
+                    const user = await new User({ facebookID: profile.id, name: profile.displayName }).save();
+                    done(null, user);
                 }
-            });
         }
     )
 );
